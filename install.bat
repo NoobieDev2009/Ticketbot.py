8097a639c5289c079b5d4b515e0dab3f123df156
NoobieDev2009 <107370285+NoobieDev2009@users.noreply.github.com>
2022-06-13 19:31:36 +0100

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,14 @@
+@echo off
+py -m pip install discord.ext
+py -m pip install requests
+py -m pip install json
+py -m pip install pathlib
+py -m pip install subprocess
+py -m pip install threading
+py -m pip install discord
+py -m pip install asyncio
+py -m pip install random
+py -m pip install ctypes
+py -m pip install re
+py -m pip install os
+pause
